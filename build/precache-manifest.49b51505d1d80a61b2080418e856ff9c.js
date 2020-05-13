da8d6f4cfca9a37185292b47ce9abb86cdcaf453
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-05-13 11:15:58 +0200

    chore: add build

---
+++ build/precache-manifest.49b51505d1d80a61b2080418e856ff9c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d890407c25737af22a0c777ca9ea95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e015059dcee95384942",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "15acfafc0a87f1d4d3cf",
+    "url": "/static/js/2.853d7e8e.chunk.js"
+  },
+  {
+    "revision": "c7f92eccde8e496c47f87d08777731a5",
+    "url": "/static/js/2.853d7e8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e015059dcee95384942",
+    "url": "/static/js/main.1b5658d1.chunk.js"
+  },
+  {
+    "revision": "77c0d1456885d70941ae",
+    "url": "/static/js/runtime-main.cf8e4e4e.js"
+  },
+  {
+    "revision": "374ec483e35043ba5ac254f3a85e212d",
+    "url": "/static/media/ChevronDown.374ec483.svg"
+  },
+  {
+    "revision": "483cfb757091b5cc4d23a6a53e2f84b4",
+    "url": "/static/media/LogoShort.483cfb75.svg"
+  },
+  {
+    "revision": "72129afb5f095a0ca89b7b824d3fcfcb",
+    "url": "/static/media/LogoTall.72129afb.svg"
+  }
+]);
