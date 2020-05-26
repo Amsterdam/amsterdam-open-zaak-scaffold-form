f2d329a859ff734cde12b029555ce43f29cb1dda
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-05-26 12:13:11 +0200

    chore: export FormPositioner

--- src/package.ts
+++ src/package.ts
@@ -1,6 +1,9 @@
 import { fetchSchema } from "./openAPIParser/fetchSchema"
 import { getSchemaObjects } from "./openAPIParser/getSchemaObjects"
-import { parseOpenApiSchema } from './openAPIParser/parseOpenApiSchema'
+import { parseOpenApiSchema } from "./openAPIParser/parseOpenApiSchema"
+
+import { FormPositioner } from "./grid/FormPositioner"
+
 
 /**
  * NOTE: These functions are exported as a NPM module.
@@ -9,6 +12,8 @@ import { parseOpenApiSchema } from './openAPIParser/parseOpenApiSchema'
 export {
   fetchSchema,
   getSchemaObjects,
-  parseOpenApiSchema
+  parseOpenApiSchema,
+
+  FormPositioner
 }
 
