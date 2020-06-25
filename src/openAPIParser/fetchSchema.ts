256215ff93ca8366a9c9d4d04d68c8098caf3476
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-06-25 16:01:03 +0200

    chore: increase timeout

--- src/openAPIParser/fetchSchema.ts
+++ src/openAPIParser/fetchSchema.ts
@@ -5,7 +5,7 @@ import {OpenAPIObject} from "openapi3-ts";
 export const fetchSchema = async (url:string) => {
   const headers = {'accept': 'application/json'}
 
-  const { data } = await axios.get(url, { headers });
+  const { data } = await axios.get(url, { headers, timeout: 30000 });
 
 
   return await $RefParser.dereference(data) as unknown as OpenAPIObject
