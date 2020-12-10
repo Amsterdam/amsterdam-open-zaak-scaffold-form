7f7f6f3d37a0c657a86fbbf2a2c1f7d20539fca6
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-12-10 11:05:54 +0100

    Fix for all caps strings

--- src/utils/humanize.ts
+++ src/utils/humanize.ts
@@ -2,9 +2,9 @@ export const humanize = (str:string) =>
   str
     .replace(/\./g, ' ')                                          // replace '.' with ' '
     .replace(/_/g, ' ')                                           // replace '_' with ' '
-    .replace(/([A-Z])/g, ' $1')                                   // camelCase -> Camel Case
+    .replace(/([a-z])([A-Z])/g, '$1 $2')                          // camelCase -> camel Case
     .toLowerCase()                                                // Lowercase everything
-    .replace(/^./, function(str){ return str.toUpperCase(); })    // uppercase first letter
+    .replace(/^./, str => str.toUpperCase())    // uppercase first letter
 
 export const humanizeOptions = (options:Record<string, string>) =>
   Object
